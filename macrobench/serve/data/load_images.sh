1da1ed240758d7761b777722da376cb3ea35ff3d
Jaewan <dominico1004@hanmail.net>
2022-12-08 01:11:27 +0000

    .

--- macrobench/serve/data/load_images.sh
+++ macrobench/serve/data/load_images.sh
@@ -1,3 +1,2 @@
 wget https://www.pixelstalk.net/wp-content/uploads/2016/03/Double-Cat-Wallpaper.jpg
 wget https://wallpapercave.com/dwp2x/qVnC9UJ.jpg
-wget https://images.cocodataset.org/annotations/annotations_trainval2017.zip
--- macrobench/serve/ensemble_serving.py
+++ macrobench/serve/ensemble_serving.py
@@ -1,36 +1,36 @@
-import ray                                                                                               
-from models import *                                                                                     
-from datasets import load_dataset                                                                        
-import random                                                                                            
-import numpy as np 
-                                                                                                         
-params=0                                                                                                 
-dataset = 0                                                                                              
-def get_params():                                                                                        
-    import argparse                                                                                      
-    global params                                                                                                                                                                                                  
-    parser = argparse.ArgumentParser()                                                                   
-    parser.add_argument('--NUM_BATCHES', '-nb', type=int, default=1)                                     
-    parser.add_argument('--BATCH_SIZE', '-bs', type=int, default=1)                                      
-    parser.add_argument('--BATCH_INTERVAL', '-bi', type=int, default=1)                                  
-    parser.add_argument('--RESULT_PATH', '-r', type=str, default="../data/dummy.csv")                    
+import ray
+from models import *
+from datasets import load_dataset
+import random
+import numpy as np
+
+params=0
+dataset = 0
+def get_params():  
+    import argparse 
+    global params
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--NUM_BATCHES', '-nb', type=int, default=1)
+    parser.add_argument('--BATCH_SIZE', '-bs', type=int, default=100)
+    parser.add_argument('--BATCH_INTERVAL', '-bi', type=int, default=1)
+    parser.add_argument('--RESULT_PATH', '-r', type=str, default="../data/dummy.csv")
     parser.add_argument('--OBJECT_STORE_SIZE', '-o', type=int, default=1_000_000_000)
-    args = parser.parse_args()                                                                           
-    params = vars(args)                                                                                  
+    parser.add_argument('--MAX_MODEL_RUN', '-m', type=int, default=10)
+    args = parser.parse_args()
+    params = vars(args) 
 
 @ray.remote
 def get_image():
-    ''' 
-        image = dataset["test"]["image"][0] 
-
-        url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
-        image = Image.open(requests.get(url, stream=True).raw)
-        url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
-        image = Image.open(requests.get(url, stream=True).raw)
-    '''
     from PIL import Image
-    image = Image.open(r'/home/ubuntu/OSDI23/macrobench/serve/data/Double-Cat-Wallpaper.jpg')
-    #image = dataset["test"]["image"][0]
+    idx = random.randint(0, 3)
+    if idx == 0:
+        image = Image.open(r'/home/ubuntu/OSDI23/macrobench/serve/data/korean-flag.jpg')
+    elif idx == 1:
+        image = Image.open(r'/home/ubuntu/OSDI23/macrobench/serve/data/Double-Cat-Wallpaper.jpg')
+    elif idx == 2:
+        image = Image.open(r'/home/ubuntu/OSDI23/macrobench/serve/data/qVnC9UJ.jpg')
+    elif idx == 3:
+        image = dataset["test"]["image"][0]
     return image
 
 @ray.remote
@@ -63,22 +63,23 @@ def initialize():
     spill_dir = os.getenv('RAY_SPILL_DIR')
     if spill_dir:
         ray.init(_system_config={"object_spilling_config": json.dumps({"type": "filesystem",
-                                    "params": {"directory_path": spill_dir}},)})
+                                    "params": {"directory_path": spill_dir}},)}, object_store_memory=params['OBJECT_STORE_SIZE'])
         print("Ray spill dir set")
     else:
-        ray.init()
+        ray.init(object_store_memory=params['OBJECT_STORE_SIZE'])
         print("Ray default init")
 
     # Load image libraries
+    random.seed(0)
     global dataset
     dataset = load_dataset("huggingface/cats-image")
 
 def get_arbitrary_model():
     #return img_models[random.randint(0, num_models)]
-    return random.randint(0, 1)
+    return img_models[random.randint(0, len(img_models) - 1)]
 
 
-@ray.remote
+@ray.remote(num_cpus=1)
 def aggregator(img, seq):
     random.seed(seq)
     INITIAL_MODEL_BATCH = 3
@@ -88,7 +89,10 @@ def aggregator(img, seq):
     predictions = {}
     for _ in range(INITIAL_MODEL_BATCH):
         m = get_arbitrary_model()
-        prediction_ref.append(m.predict.remote(img))
+        if _ == 0:
+            prediction_ref.append(m.predict.remote(processed_img))
+        else:
+            prediction_ref.append(m.predict.remote(img))
     num_models_run = INITIAL_MODEL_BATCH
 
     for i in range(INITIAL_MODEL_BATCH):
@@ -98,13 +102,18 @@ def aggregator(img, seq):
             return num_models_run
 
     vote = 0
-    while vote < ((num_models_run//2) + 1):
+    while vote < ((num_models_run//2) + 1) or num_models_run > params['MAX_MODEL_RUN']:
         m = get_arbitrary_model()
-        pred = ray.get(m.predict.remote(img))
+        original_image = random.randint(0, params['MAX_MODEL_RUN']//2)
+        pred = 0
+        if original_image:
+            pred = ray.get(m.predict.remote(img))
+        else:
+            pred = ray.get(m.predict.remote(processed_img))
+
         num_models_run += 1
         predictions[pred] = predictions.get(pred,0) + 1
         vote = predictions[pred]
-
 
     return num_models_run
 
@@ -124,12 +133,6 @@ if __name__ == '__main__':
     from time import perf_counter
 
     initialize()
-    
-    img = get_image.remote()
-    import time
-    import os
-    time.sleep(1)
-    os.system('ray memory --stats-only')
 
     # Load Images
 
@@ -138,32 +141,44 @@ if __name__ == '__main__':
     num_models = len(MODELS)
     img_models = []
     img_models.append(Resnet18.remote())
-    img_models.append(BEiT.remote())
     img_models.append(Resnet50.remote())
     img_models.append(Resnet101.remote())
+    img_models.append(BEiT.remote())
     img_models.append(ConvNeXT.remote())
     img_models.append(ViT384.remote())
+    img_models.append(MIT_B0.remote())
 
-    res = []
-    for model in img_models:
-        res.append(model.predict.remote(img))
-
-    for r in res:
-        print(ray.get(r))
-
-    img = preprocess.remote(img)
-    res = []
-    for model in img_models:
-        res.append(model.predict.remote(img))
-
-    for r in res:
-        print(ray.get(r))
-    
     '''
+    
+    img = get_image.remote()
+    import time
+    import os
+    time.sleep(1)
+    os.system('ray memory --stats-only')
+    processed_img = preprocess.remote(img)
+    time.sleep(1)
+    os.system('ray memory --stats-only')
+    res = []
+    for model in img_models:
+        res.append(model.predict.remote(img))
+
+    for r in res:
+        print(ray.get(r))
+
+    res = []
+    for model in img_models:
+        res.append(model.predict.remote(processed_img))
+
+    for r in res:
+        print(ray.get(r))
+   
+    '''
+    import os
     res = []
     start = perf_counter()
     for _ in range(params['NUM_BATCHES']):
         res.append(batch_submitter())
+    os.system('ray memory --stats-only')
 
     models_run = 0
     for i in range(len(res)):
@@ -172,4 +187,3 @@ if __name__ == '__main__':
     end = perf_counter()
 
     print(f"{params['NUM_BATCHES']} batches with {params['BATCH_SIZE']} requests : {end-start} ran {models_run} models")
-    '''
