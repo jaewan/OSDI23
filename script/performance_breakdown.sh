41c777c78eaeb1148852bb20302337a843f10089
Jaewan Hong <dominico1004@hanmail.net>
2022-12-12 20:25:36 +0000

    .

--- script/performance_breakdown.sh
+++ script/performance_breakdown.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-DEBUG=true
+DEBUG=false
 
 ################ System Variables ################ 
 APPLICATION=shuffle
-LOG_DIR=../data/$APPLICATION\_
+LOG_DIR=../data/$APPLICATION/
 TEST_FILE=../microbench/instantSubmission/$APPLICATION.py
 OBJECT_STORE_SIZE=4000000000
 OBJECT_SIZE=100000000
@@ -12,21 +12,22 @@ OBJECT_SIZE=100000000
 ################ Test Techniques ################ 
 Production_RAY=false
 OFFLINE=false
-DFS=true
+DFS=false
 DFS_EVICT=false
-DFS_BACKPRESSURE=false
-DFS_BLOCKSPILL=false
+DFS_BACKPRESSURE=true
+DFS_BLOCKSPILL=true
 DFS_EVICT_BLOCKSPILL=false
-DFS_BACKPRESSURE_BLOCKSPILL=false
-EAGERSPILL=false
+DFS_BACKPRESSURE_BLOCKSPILL=true
+DFS_EAGERSPILL=true
 
 function Test()
 {
 	BACKPRESSURE=$1
 	BLOCKSPILL=$2
 	EVICT=$3
-	OFF=$4
-	RESULT_FILE=$LOG_DIR$5.csv
+	EAGERSPILL=$4
+	OFF=$5
+	RESULT_FILE=$LOG_DIR$6.csv
 	if $DEBUG;
 	then
 		#for w in {1,2,4,8}
@@ -37,7 +38,7 @@ function Test()
 			RAY_BACKEND_LOG_LEVEL=debug \
 			RAY_block_tasks_threshold=1.0 RAY_object_spilling_threshold=1.0 RAY_spill_wait_time=$a RAY_enable_Deadlock2=true \
 			RAY_enable_BlockTasks=$BACKPRESSURE  RAY_enable_EvictTasks=$EVICT RAY_enable_BlockTasksSpill=$BLOCKSPILL RAY_enable_EagerSpill=$EAGERSPILL\
-			python $TEST_FILE -w $w -o $OBJECT_STORE_SIZE -os $OBJECT_SIZE -t 10 -nw 40
+			python $TEST_FILE -w $w -o $OBJECT_STORE_SIZE -os $OBJECT_SIZE -t 1 -nw 40
 		#done
 	else
 		test -f "$RESULT_FILE" && rm $RESULT_FILE
@@ -45,10 +46,10 @@ function Test()
 		for w in {1,2,4,8}
 		do
 			let a=50000
-			echo $5 -w $w 
-			RAY_block_tasks_threshold=1.0 RAY_object_spilling_threshold=1.0 RAY_spill_wait_time=$a RAY_enable_Deadlock2=true \
+			echo $6 -w $w 
+			RAY_block_tasks_threshold=1.0 RAY_object_spilling_threshold=1.0 RAY_spill_wait_time=$a RAY_enable_Deadlock2=$BLOCKSPILL \
 			RAY_enable_BlockTasks=$BACKPRESSURE  RAY_enable_EvictTasks=$EVICT RAY_enable_BlockTasksSpill=$BLOCKSPILL RAY_enable_EagerSpill=$EAGERSPILL\
-			python $TEST_FILE -w $w -r $RESULT_FILE -o $OBJECT_STORE_SIZE -os $OBJECT_SIZE  -off $OFF -t 5 -nw 60 
+			python $TEST_FILE -w $w -r $RESULT_FILE -o $OBJECT_STORE_SIZE -os $OBJECT_SIZE  -off $OFF -t 5 -nw 32 
 		done
 		rm -rf /tmp/ray/*
 	fi
@@ -56,51 +57,55 @@ function Test()
 
 if $Production_RAY;
 then
-	Test false false false false RAY
+	#./../script/install/install_production_ray.sh
+	Test false false false false false RAY
 fi
 
 if $DFS;
 then
-	Test false false false false DFS
+	#./../script/install/install_boa.sh
+	Test false false false false false DFS
 fi
 
 if $OFFLINE;
 then
-	Test false false false true OFFLINE
+	Test false false false false true OFFLINE
 fi
 
 if $DFS_EVICT;
 then
-	Test false false true false DFS_Evict
+	Test false false true false false DFS_Evict
 fi
 
 if $DFS_BACKPRESSURE;
 then
-	Test true false false false DFS_Backpressure
+	Test true false false false false DFS_Backpressure
 fi
 
 if $DFS_BLOCKSPILL;
 then
-	Test false true false false DFS_BlockSpill
+	Test false true false false false DFS_BlockSpill
 fi
 
 if $DFS_EVICT_BLOCKSPILL;
 then
-	Test false true true false DFS_Evict_BlockSpill
+	Test false true true false false DFS_Evict_BlockSpill
 fi
 
 if $DFS_BACKPRESSURE_BLOCKSPILL;
 then
- 	Test true true false false DFS_Backpressure_BlockSpill_Deadlock
+ 	Test true true false false false DFS_Backpressure_BlockSpill_Deadlock
 fi
 
-if $EAGERSPILL;
+if $DFS_EAGERSPILL;
 then
- 	Test false false false false EagerSpill
+ 	Test false false false true false EagerSpill
 fi
 
 ################ Plot Graph ################ 
+<<comment
 if ! $DEBUG;
 then
 ./plot.py $APPLICATION
 fi
+comment
