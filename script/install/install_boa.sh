6c5a19271699ee0616ccb89f7fad052d9ba7cce1
Jaewan Hong <dominico1004@hanmail.net>
2023-01-12 21:43:02 +0000

    .

--- script/install/install_boa.sh
+++ script/install/install_boa.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "Install Boa Ray"
-pushd /home/ubuntu/ray_memory_management/python
+pushd $HOME/ray_memory_management/python
 pip uninstall ray --y
 pip install -e . --verbose
 popd
--- script/install/install_production_ray.sh
+++ script/install/install_production_ray.sh
@@ -2,6 +2,6 @@
 
 echo "Install Production Ray"
 pip uninstall ray --y
-pushd /home/ubuntu/production_ray/python
+pushd $HOME/production_ray/python
 pip install -e . --verbose
 popd
