03b6c246fb8b72d2d5cda46ff0c670409c5b8f27
Jaewan Hong <dominico1004@hanmail.net>
2023-03-12 23:40:32 +0000

    up script with options

--- script/multinode/up.sh
+++ script/multinode/up.sh
@@ -3,6 +3,8 @@
 PORT=6379
 HEAD=true
 HEAD_ADDR=35.199.189.234
+NUM_CPUS=4
+OBJECT_STORE_MEMORY_SIZE=1000000000
 
 function Start_Ray()
 {
@@ -11,8 +13,9 @@ function Start_Ray()
 	then
 		RAY_UP_COMMAND="ray start --head --port=$PORT"
 	fi
-	$RAY_UP_COMMAND --system-config=‘{“object_spilling_config”:“{\“type\“:\“filesystem\“,\“params\“:{\“directory_path\“:\“$(RAY_SPILL_DIR)\“}}“}’
+	$RAY_UP_COMMAND --system-config='{"object_spilling_config":"{\"type\":\"filesystem\",\"params\":{\"directory_path\":\"${RAY_SPILL_DIR}\"}}"}'\
+		--num-cpus $NUM_CPUS --object-store-memory $OBJECT_STORE_MEMORY_SIZE
+
 }
 
-#Start_Ray
-echo $RAY_SPILL_DIR
+Start_Ray
