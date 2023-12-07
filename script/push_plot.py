d9f2a63f70dbee53ebde112424138c07c080050a
Jaewan Hong <dominico1004@hanmail.net>
2023-12-07 22:30:46 +0000

    .

---
+++ script/push_plot.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Data on X-axis
+
+colors = ['#10739E','#4D3700']
+boa_data = [268, 263, 248]
+ray_data = [420, 375, 310]
+
+boa_restored = [0,0,0.5,1.8]
+ray_restored = [0,0,20,76]
+
+# Position of bars on x-axis
+ind = np.arange(len(boa_data))
+
+# Figure size
+plt.figure(figsize=(20,10))
+plt.grid(zorder=0, axis='y', color='grey')
+
+# Width of a bar 
+width = 0.3       
+
+# Plotting
+ray_bar = plt.bar(ind, ray_data , width, label='Production Ray', color=colors[0], zorder=3)
+boa_bar = plt.bar(ind + width, boa_data, width, label='Boa', color=colors[1], zorder=3)
+
+'''
+i = 0
+for bar in ray_bar:
+    height = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width() / 2.0, height, f'{ray_data[i]:.0f}', ha='center', va='bottom')
+    i = i + 1
+
+i = 0
+for bar in boa_bar:
+    height = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width() / 2.0, height, f'{boa_data[i]:.0f}', ha='center', va='bottom')
+    i = i + 1
+'''
+
+
+#plt.xlabel('Number of NYC Taxi Files per Node', fontsize = 35)
+plt.ylabel('Runtime (sec)', fontsize = 35)
+#plt.title('title')
+
+# xticks()
+# First argument - A list of positions at which ticks should be placed
+# Second argument -  A list of labels to place at the given locations
+plt.xticks(ind + width / 2, ('No Execution Order','No Backpressure', 'App Scheduled'), fontsize = 20)
+plt.yticks(fontsize = 20)
+
+# Finding the best position for legends and putting it
+plt.legend(loc='best', fontsize=35)
+plt.savefig("push_based_shuffle.png")
